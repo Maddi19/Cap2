4822ff9ee3297edcacaec34c43d04bff894110c0
Maddi19 <maddiart19@gmail.com>
2024-03-13 12:27:53 +0100

    LL

--- scripts/G22_cleaning.R
+++ scripts/G22_cleaning.R
@@ -42,19 +42,61 @@ d.g2$Polinizador<-recode(d.g2$Polinizador, "?=28"= "Scaeva pyrastri",
                          "?=17"="Eupeodes corollae",
                          "?=20" = "Andrena propinqua",
                          "?=18" = "Bombus soroeensis",
-                         "?=60" =
-                          "?=30"= "Eupeodes corollae",
-                           "?=24"= "Andrena nigroaenea",
-                           "?=29"= "Cheilosia velutina",
-                           "?=37" = "Bombus soroeensis",
+                         "?=60" = "Eupeodes corollae",
+                         "?=30"= "Eupeodes corollae",
+                         "?=24"= "Andrena nigroaenea",
+                         "?=29"= "Cheilosia velutina",
+                         "?=37" = "Bombus soroeensis",
                          "?=35"="Apis mellifera",
                          "?=36"="Apis mellifera",
                          "?=41"="Andrena nigroaenea",
                          "?=44"="Lasioglossum sp",
                          "?=45"="Andrena nigroaenea",
-                         "?=49" =
-                          "?=52" =
-                           "?=54"= 
+                         "?=49" = "Platycheirus albimanus",
+                         "?=52" = "Calliphora vicina",
+                         "?=54"= "Lasioglossum immunitum",
+                         "?=56"="Bombus ruderarius",
+                         "?=57"= "Andrena cineraria",
+                         "?=58"="Cheilosia nigripes",
+                         "?=61" ="Lasioglossum sp",
+                         "?=66" ="Andrena propinqua",
+                         "?=64"= "Merodon sp.",
+                         "?=68" = "Melangyna sp.",
+                         "?=59" = "Andrena propinqua",
+                         "?=75" = "Eucera nigrescens",
+                         "?=73" = "Andrena cineraria",
+                         "?=80" = "Lasioglossum sp",
+                         "?=89" = "Apis mellifera",                              
+                         "?=91" = "Osmia emarginata",                                 
+                         "?=93" = "Andrena ovatula",                                 
+                         "?=94" = "Halictus confusus",
+                         "?=97" = "Platycheirus albimanus",
+                         "?=99" = "Syrphidae",
+                         "?=100"= "Syrphus sp.",
+                         "?=106"="Bombus sylvarum",
+                         "?=110"="Andrena minutula",
+                         "?=115"="Andrena ovatula",
+                         "?=116"=" Syrphus sp.",
+                         "?=118"="Bombus hortorum",
+                         "?=123"= "Halictus sp")
+                         
+                           
+#remove some with no id
+d.g3<-d.g2[!(d.g2$Polinizador=="?=1"|d.g2$Polinizador=="?=3"|d.g2$Polinizador=="?=9"|d.g2$Polinizador=="11"|d.g2$Polinizador=="21"|
+               d.g2$Polinizador=="31"|d.g2$Polinizador=="?=32"),]
+
+                           
+###create genus column
+library("stringr")
+d.g4<- d.g3%>%
+  mutate(Pollinator_genus= word(Polinizador, 1))
+unique(d.g4$Pollinator_genus)    
+
+###family names
+d.g4$Pollinator_family[d.g4$Pollinator_genus=="Apis"] <- "Apidae"
+d.g4$Pollinator_family[d.g4$Pollinator_genus=="Syrphidae"] <- "Syrphidae"
+                         
+                         
                            
                            
                         
